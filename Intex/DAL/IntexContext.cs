f9dbb11e37d5a46b4a02a4e814c1069d87af469f
Evan McDaniel <eamigo13@gmail.com>
2017-12-04 10:30:09 -0700

    Added connnection string and DAL folder/DbContext file

---
+++ Intex/DAL/IntexContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Intex.DAL
+{
+    public class IntexContext : DbContext
+    {
+        public IntexContext() : base("IntexContext")
+        {
+
+        }
+    }
+}
