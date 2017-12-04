3a491a9da67417ee42a83424c388482b89be02c3
Evan McDaniel <eamigo13@gmail.com>
2017-12-04 09:26:01 -0700

    Add project files.

---
+++ Intex/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Intex.Startup))]
+namespace Intex
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
