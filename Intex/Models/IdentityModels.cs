ab6c831a1902f8a1261f94878dc809586d22dd28
Riley Wells <rileymorganwells@gmail.com>
2017-12-05 15:01:47 -0700

    Moved identitymodels database to the CLOUD, and changed identityusers connection in identitymodels model, now everything is in the freakin cloud!

--- Intex/Models/IdentityModels.cs
+++ Intex/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Intex.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("IntexContext", throwIfV1Schema: false)
         {
         }
 
