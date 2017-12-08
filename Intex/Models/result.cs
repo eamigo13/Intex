1012b2909f63831e3d35ac1ad980ad9596e0209b
Andrew Marquez <andy2mgcc@gmail.com>
2017-12-08 00:10:07 -0700

    Finished the autogenerated reports page and added a view with buttons for our proposal

---
+++ Intex/Models/result.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Intex.Models
+{
+    [Table("Results")]
+    public class Result
+    {
+        [Key]
+        public int TestID { get; set; }
+        public string PassFail { get; set; }
+        public string Comments { get; set; }
+
+        [ForeignKey("TestID")]
+        public virtual Test test { get; set; }
+    }
+}
